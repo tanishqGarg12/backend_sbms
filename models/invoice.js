a4d2ac9f5562f2406b702d66b1e97a41449d3b67
tanishq <tanishqgarg256@gmail.com>
2024-09-04 21:16:06 +0530

    commiting after ce1

--- models/invoice.js
+++ models/invoice.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 const invoiceSchema = new mongoose.Schema({
     senderId: { type: String, required: true },
-    senderName: { type: String, required: true },
-    senderContact: { type: String, required: true },
-    senderEmail: { type: String, required: true },
-    recipientName: { type: String, required: true },
-    recipientContact: { type: String, required: true },
-    recipientEmail: { type: String, required: true },
+        senderName: { type: String, required: true },
+        senderContact: { type: String, required: true },
+        senderEmail: { type: String, required: true },
+        recipientName: { type: String, required: true },
+        recipientContact: { type: String, required: true },
+        recipientEmail: { type: String, required: true },
     services: [
         {
             name: { type: String, required: true },
