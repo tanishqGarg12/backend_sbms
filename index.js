d90bf17c878f441364b0f065bb07262f064bb21a
tanishq <tanishqgarg256@gmail.com>
2024-12-29 23:39:28 +0530

    adding all

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const path = require('path');
+const database = require("./config/database");
+
+// Routes
 const userRoutes = require("./routers/User");
 const inventoryRoutes = require("./routers/inventory");
 const invoiceRoutes = require("./routers/invoice");
@@ -7,44 +14,36 @@ const testimonialRoutes = require("./routers/testimonialRoutes");
 const cartRoutes = require("./routers/cart");
 const categoryRoutes = require("./routers/categorySubcategoryRoutes");
 const paymentRoutes = require("./routers/paymentRoutes");
-const database = require("./config/database");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-// const upload=require('./uploads')
-const path = require('path');
-app.use('/uploads', express.static(path.join(__dirname, './uploads')));
-
 
 dotenv.config();
-
-const PORT = 4000;
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 // Database connection
 database.connect();
 
+// CORS Configuration
 app.use(cors({
-    origin: 'http://localhost:3000', // Your frontend domain
+    origin: ['https://sbms1-tanishqgarg12s-projects.vercel.app'], // Frontend domain
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie'],
 }));
+app.options('*', cors());
 
-// Middlewares
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
+// Razorpay Instance
 const Razorpay = require("razorpay");
 const instance = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_API_SECRET,
 });
-
-console.log(process.env.RAZORPAY_API_KEY);
-console.log(process.env.RAZORPAY_API_SECRET);
-
-// Use the Razorpay instance within relevant routes or controllers
-// module.exports = instance;
 
 // Routes
 app.use("/api/v1/auth", userRoutes);
@@ -55,7 +54,7 @@ app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/categoryy", categoryRoutes);
 app.use("/api/v1/pay", paymentRoutes);
 
-// Default route
+// Default Route
 app.get("/", (req, res) => {
     return res.json({
         success: true,
@@ -65,5 +64,5 @@ app.get("/", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`App is running at http://localhost:${PORT}`);
+    console.log(`App is running on port ${PORT}`);
 });
